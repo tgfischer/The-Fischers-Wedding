7235a1f0e48630708673adf2cca6ab94178a3f95
Tom Fischer <tgfischer6@gmail.com>
2021-08-15 22:05:28 -0400

    fixed alignment

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -6,22 +6,17 @@ import { Page } from "../Page";
 export const Home: NextPage = () => (
   <Page
     style={{
-      background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/images/bg.jpg") center center / cover no-repeat;`
+      background: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("/images/bg.jpg") center center / cover no-repeat`
     }}
   >
     <div className="vh-100 d-flex align-items-center text-white">
       <Container fluid>
         <Row>
-          {/* <Col className="d-flex justify-content-center" xs={12}>
-          <p className="lead text-uppercase" style={{ fontSize: "1rem" }}>
-            please join us for the wedding celebration of
-          </p>
-        </Col> */}
           <Col
             className="d-flex justify-content-center align-items-center my-3"
             xl={12}
           >
-            <h1 className="handwritten display-1">Tom Fischer</h1>
+            <h1 className="handwritten display-1 text-end">Tom Fischer</h1>
             <h1 className="handwritten px-3">and</h1>
             <h1 className="handwritten display-1">Megan Heikens</h1>
           </Col>
