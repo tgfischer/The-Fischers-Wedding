c3e21d4bf2f275f5c3143f8d247e9c3f94ae3f0a
Tom Fischer <tgfischer6@gmail.com>
2021-09-13 15:49:07 -0400

    changed osm zoom level

--- src/components/SetReservationPage/Location.tsx
+++ src/components/SetReservationPage/Location.tsx
@@ -15,7 +15,7 @@ export const Location = (): JSX.Element => {
         <h2 className="handwritten display-5">Location</h2>
         <Row>
           <Col md>
-            <OpenStreetMap center={[44.18088, -81.6389]} zoom={18}>
+            <OpenStreetMap center={[44.18088, -81.6389]} zoom={17}>
               <OpenStreetMapMarker position={[44.18045, -81.63895]} />
               <OpenStreetMapMarker position={[44.18124, -81.63895]} />
             </OpenStreetMap>
