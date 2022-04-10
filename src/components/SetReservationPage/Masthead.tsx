8741162cc0c36ae7251d999ea5861974d2f507d3
tgfischer <tgfischer6@gmail.com>
2022-04-10 11:00:49 -0400

    Add dinner and reception option

--- src/components/SetReservationPage/Masthead.tsx
+++ src/components/SetReservationPage/Masthead.tsx
@@ -10,7 +10,7 @@ type MastheadProps = {
 
 const CeremonyDinnerReceptionInvitation = () => (
   <>
-    <p className="text-uppercase lead mb-4">{"4:00 in the afternoon"}</p>
+    <p className="text-uppercase lead mb-4">4:00 in the afternoon</p>
     <h3 className="handwritten display-6 mb-1">Kincardine Rock Gardens</h3>
     <p className="text-uppercase lead m-0">
       155 Durham Street, Kincardine Ontario, N2Z 1A4
@@ -19,9 +19,20 @@ const CeremonyDinnerReceptionInvitation = () => (
   </>
 );
 
+const DinnerReceptionInvitation = () => (
+  <>
+    <p className="text-uppercase lead mb-4">6:00 in the evening for dinner</p>
+    <h3 className="handwritten display-6 mb-1">Kincardine Pavilion</h3>
+    <p className="text-uppercase lead m-0">
+      156 Durham Street, Kincardine Ontario, N2Z 1A4
+    </p>
+    <p className="text-uppercase">Reception to follow</p>
+  </>
+);
+
 const ReceptionInvitation = () => (
   <>
-    <p className="text-uppercase lead mb-4">{"9 o'clock in the evening"}</p>
+    <p className="text-uppercase lead mb-4">9:00 in the evening</p>
     <h3 className="handwritten display-6 mb-1">Kincardine Pavilion</h3>
     <p className="text-uppercase lead m-0">
       156 Durham Street, Kincardine Ontario, N2Z 1A4
@@ -33,6 +44,10 @@ const getInvitation = cond<ReservationDto, () => JSX.Element>([
   [
     propEq("invitations", ["ceremony", "dinner", "reception"]),
     always(CeremonyDinnerReceptionInvitation)
+  ],
+  [
+    propEq("invitations", ["dinner", "reception"]),
+    always(DinnerReceptionInvitation)
   ],
   [T, always(ReceptionInvitation)]
 ]);
